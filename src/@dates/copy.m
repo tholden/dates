b4ed9ad6f74f3ed4b2fe30f9bf85c417cb7284ff
Stéphane Adjemian (Charybdis) <stephane.adjemian@univ-lemans.fr>
2014-12-05 16:35:24 +0100

    Added (deep) copy method.

---
+++ src/@dates/copy.m
@@ -0,0 +1,29 @@
+function p = copy(o)
+
+% Do a copy of a dates object.
+%
+% INPUTS
+% - o [dates]
+%
+% OUTPUTS
+% - p [dates]
+
+% Copyright (C) 2014 Dynare Team
+%
+% This code is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% Dynare dates submodule is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
+
+p = dates();
+p.freq = o.freq;
+p.ndat = o.ndat;
+p.time = o.time;
