8d4f47e36175893ed6d25004b3bbcc2a08ac0dfc
Stéphane Adjemian (Charybdis) <stephane.adjemian@univ-lemans.fr>
2014-12-06 22:30:53 +0100

    Rewrote uminus and uplus methods.

--- src/@dates/uplus.m
+++ src/@dates/uplus.m
@@ -1,23 +1,21 @@
-function B = uplus(A)  % --*-- Unitary tests --*--
+function o = uplus(o)  % --*-- Unitary tests --*--
 
 % Overloads the unary plus operator for dates objects. Shifts all the elements by one period.
 %
 % INPUTS 
-%  o A    dates object with n elements.
+% - o [dates]
 %
 % OUTPUTS 
-%  o B    dates object with n elements.
+% - o [dates]
 
-% Copyright (C) 2013 Dynare Team
+% Copyright (C) 2013-2014 Dynare Team
 %
-% This file is part of Dynare.
-%
-% Dynare is free software: you can redistribute it and/or modify
+% This code is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
 %
-% Dynare is distributed in the hope that it will be useful,
+% Dynare dates submodule is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
@@ -25,11 +23,10 @@ function B = uplus(A)  % --*-- Unitary tests --*--
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
 
-B = dates(A);
-B.time(:,2) = B.time(:,2)+1;
-idx = find(B.time(:,2)>B.freq); % Happy new year!
-B.time(idx,1) = B.time(idx,1)+1;
-B.time(idx,2) = 1;
+o.time(:,2) = o.time(:,2)+1;
+idx = find(o.time(:,2)>o.freq); % Happy new year!
+o.time(idx,1) = o.time(idx,1)+1;
+o.time(idx,2) = 1;
 
 %@test:1
 %$ % Define some dates
@@ -40,11 +37,11 @@ B.time(idx,2) = 1;
 %$ date_5 = '1950M12';
 %$
 %$ % Call the tested routine.
-%$ d1 = dates(date_1); d1 = +d1;
-%$ d2 = dates(date_2); d2 = +d2;
-%$ d3 = dates(date_3); d3 = +d3;
-%$ d4 = dates(date_4); d4 = +d4;
-%$ d5 = dates(date_5); d5 = +d5;
+%$ d1 = dates(date_1); +d1;
+%$ d2 = dates(date_2); +d2;
+%$ d3 = dates(date_3); +d3;
+%$ d4 = dates(date_4); +d4;
+%$ d5 = dates(date_5); +d5;
 %$ i1 = (d1==dates('1951Y'));
 %$ i2 = (d2==dates('1950Q3'));
 %$ i3 = (d3==dates('1951Q1'));
@@ -64,14 +61,14 @@ B.time(idx,2) = 1;
 %$ d1 = dates('1950Q1','1950Q2','1950Q3','1950Q4','1951Q1');
 %$ d2 = dates('1950Q2','1950Q3','1950Q4','1951Q1','1951Q2');
 %$ try
-%$   d3 = +d1;
+%$   +d1;
 %$   t(1) = 1;
 %$ catch
 %$   t(1) = 0;
 %$ end
 %$
 %$ if t(1)
-%$   t(2) = dassert(all(d2==d3),true);
+%$   t(2) = dassert(all(d2==d1),true);
 %$ end
 %$
 %$ T = all(t);
