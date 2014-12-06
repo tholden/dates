26074ce205c8d343094564b05e93964b6695614b
Stéphane Adjemian (Charybdis) <stephane.adjemian@univ-lemans.fr>
2014-12-06 21:26:07 +0100

    Rewrote setdiff function.

--- src/@dates/setdiff.m
+++ src/@dates/setdiff.m
@@ -1,40 +1,24 @@
-function C = setdiff(A,B) % --*-- Unitary tests --*--
+function q = setdiff(o,p) % --*-- Unitary tests --*--
 
-%@info:
-%! @deftypefn {Function File} {@var{C} =} setdiff (@var{A},@var{B})
-%! @anchor{@dates/intersect}
-%! @sp 1
-%! C of B and A.
-%! if A and B are not disjoints.
-%! @sp 2
-%! @strong{Inputs}
-%! @sp 1
-%! @table @ @var
-%! @item A
-%! @ref{dates} object.
-%! @item B
-%! @ref{dates} object.
-%! @end table
-%! @sp 2
-%! @strong{Outputs}
-%! @sp 1
-%! @table @ @var
-%! @item C
-%! @ref{dates} object.
-%! @end table
-%! @end deftypefn
-%@eod:
+% Overloads setdiff function for dates objects.
+%
+% INPUTS 
+% - o [dates]
+% - p [dates]
+%
+% OUTPUTS 
+% - q [dates]
+%
+% See also pop, remove.
 
-% Copyright (C) 2013 Dynare Team
+% Copyright (C) 2013-2014 Dynare Team
 %
-% This file is part of Dynare.
-%
-% Dynare is free software: you can redistribute it and/or modify
+% This code is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
 %
-% Dynare is distributed in the hope that it will be useful,
+% Dynare dates submodule is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
@@ -42,34 +26,32 @@ function C = setdiff(A,B) % --*-- Unitary tests --*--
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
 
-if ~isa(A,'dates') || ~isa(B,'dates')
-    error(['dates::plus: Input arguments ''' inputname(1) ''' and ''' inputname(2) ''' must be dates objects!'])
+if ~isa(o,'dates') || ~isa(o,'dates')
+    error('dates;setdiff','All input arguments must be dates objects!')
 end
 
-if eq(A,B)
-    C = A;
-    return
+if ~isequal(o.freq,p.freq)
+    error('dates;setdiff','All input arguments must have common frequency!')
 end
 
-if ~isequal(A.freq,B.freq)
-    C = dates();
+if o==p
+    q = dates(o.freq);
     return
 end
 
 if isoctave || matlab_ver_less_than('8.1.0')
-    time = setdiff(A.time,B.time,'rows');
+    time = setdiff(o.time,p.time,'rows');
 else
-    time = setdiff(A.time,B.time,'rows','legacy');
+    time = setdiff(o.time,p.time,'rows','legacy');
 end
 
-C = dates();
+q = dates(o.freq);
 if isempty(time)
     return
 end
 
-C.freq = A.freq;
-C.time = time;
-C.ndat = rows(time); 
+q.time = time;
+q.ndat = rows(time); 
 
 %@test:1
 %$ % Define some dates objects
