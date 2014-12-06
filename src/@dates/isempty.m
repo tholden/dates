d44a5df6f7445bd77b4dee665af08df20de10d52
Stéphane Adjemian (Charybdis) <stephane.adjemian@univ-lemans.fr>
2014-12-06 13:42:12 +0100

    Rewrote isempty method.

--- src/@dates/isempty.m
+++ src/@dates/isempty.m
@@ -1,37 +1,21 @@
-function B = isempty(A) % --*-- Unitary tests --*--
+function l = isempty(o) % --*-- Unitary tests --*--
 
-%@info:
-%! @deftypefn {Function File} {@var{B} =} isempty (@var{A})
-%! @anchor{@dates/isempty}
-%! @sp 1
-%! Overloads the isempty function for the @ref{dates} class.
-%! @sp 2
-%! @strong{Inputs}
-%! @sp 1
-%! @table @ @var
-%! @item A
-%! @ref{dates} object.
-%! @end table
-%! @sp 1
-%! @strong{Outputs}
-%! @sp 1
-%! @table @ @var
-%! @item b
-%! Integer scalar (equal to zero if @var{A} is not empty).
-%! @end table
-%! @end deftypefn
-%@eod:
+% Returns true (1) if and only if o dates object is empty.
+%
+% INPUTS 
+% - o [dates]
+%
+% OUTPUTS 
+% - l [logical]
 
-% Copyright (C) 2013 Dynare Team
+% Copyright (C) 2013-2014 Dynare Team
 %
-% This file is part of Dynare.
-%
-% Dynare is free software: you can redistribute it and/or modify
+% This code is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation, either version 3 of the License, or
 % (at your option) any later version.
 %
-% Dynare is distributed in the hope that it will be useful,
+% Dynare dates submodule is distributed in the hope that it will be useful,
 % but WITHOUT ANY WARRANTY; without even the implied warranty of
 % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 % GNU General Public License for more details.
@@ -39,7 +23,7 @@ function B = isempty(A) % --*-- Unitary tests --*--
 % You should have received a copy of the GNU General Public License
 % along with Dynare.  If not, see <http://www.gnu.org/licenses/>.
     
-B = isequal(A.ndat,0);
+l = isequal(o.ndat,0);
 
 %@test:1
 %$ % Instantiate an empty dates object
@@ -48,3 +32,11 @@ B = isequal(A.ndat,0);
 %$ t(1) = isempty(d);
 %$ T = all(t);
 %@eof:1
+
+%@test:2
+%$ % Instantiate an empty dates object
+%$ d = dates('1938Q4');
+%$ % Test if this object is empty
+%$ t(1) = ~isempty(d);
+%$ T = all(t);
+%@eof:2
