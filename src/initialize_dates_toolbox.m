tholden/dates

commit 555cffc081897c3fc1f36903de79febbf620204c

Efficiency change related to the path definition.

src/initialize_dates_toolbox.m
@@ -1,28 +1,32 @@
 % Get the path to the dates/src folder.
 dates_src_root = strrep(which('initialize_dates_toolbox'),'initialize_dates_toolbox.m','');
 
-% Add some subfolders to the path.
-addpath([dates_src_root '/utilities/is'])
-addpath([dates_src_root '/utilities/op'])
-addpath([dates_src_root '/utilities/convert'])
+% Set the subfolders to be added in the path.
+p = {'/utilities/is'; ...
+     '/utilities/op'; ...
+     '/utilities/convert'};
 
 % Add missing routines if dynare is not in the path
 if ~exist('isint','file')
-    addpath([dates_src_root '/utilities/missing/isint'])
+    p{end+1} = '/utilities/missing/isint';
 end
 
 if ~exist('isoctave','file') 
-    addpath([dates_src_root '/utilities/missing/isoctave'])
+    p{end+1} = '/utilities/missing/isoctave';
 end
 
 if ~isoctave && (~exist('rows','file') || ~exist('columns','file'))
-    addpath([dates_src_root '/utilities/missing/dims'])
+    p{end+1} = '/utilities/missing/dims';
 end
 
 if ~exist('shiftS','file') 
-    addpath([dates_src_root '/utilities/missing/shiftS'])
+    p{end+1} = '/utilities/missing/shiftS';
 end
 
 if ~exist('matlab_ver_less_than','file') 
-    addpath([dates_src_root '/utilities/missing/matlab_ver_less_than'])
+    p{end+1} = '/utilities/missing/matlab_ver_less_than';
 end
+
+% Set path
+P = cellfun(@(c)[dates_src_root(1:end-1) c], p, 'uni', false);
+addpath(P{:});

commit b91c59ae232175d59616827e3c87f34a075fc6bb

Fixed bug introduced in 555cffc081897c3fc1f36903de79febbf620204c.

src/initialize_dates_toolbox.m
@@ -15,10 +15,6 @@ if ~exist('isoctave','file')
     p{end+1} = '/utilities/missing/isoctave';
 end
 
-if ~isoctave && (~exist('rows','file') || ~exist('columns','file'))
-    p{end+1} = '/utilities/missing/dims';
-end
-
 if ~exist('shiftS','file') 
     p{end+1} = '/utilities/missing/shiftS';
 end
@@ -30,3 +26,7 @@ end
 % Set path
 P = cellfun(@(c)[dates_src_root(1:end-1) c], p, 'uni', false);
 addpath(P{:});
+
+if ~isoctave && (~exist('rows','file') || ~exist('columns','file'))
+    addpath([dates_src_root(1:end-1) '/utilities/missing/dims']);
+end